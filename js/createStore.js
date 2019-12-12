be72991ca6c3dae026715ee3e25bf8fc6a1405ba
Liam <liam.healy1@marist.edu>
2019-12-12 11:33:46 -0500

    Done.

--- js/createStore.js
+++ js/createStore.js
@@ -1,4 +1,24 @@
-let state;
+function createStore(reducer) {
+  let state
+
+  function dispatch(action){
+    state = reducer(state, action);
+    render();
+  };
+
+  function getState() {
+    return state
+  }
+
+  return { dispatch, getState }
+}
+
+// Note: You may notice that in the above code we made a closure.
+// As you surely remember a JavaScript function has access to all 
+// the variables that were in scope at the time of its definition.
+// This feature is called a closure since a function encloses or 
+// draws a protective bubble around the variables in its scope and
+// carries those with it when invoked later.
 
 function reducer(state = { count: 0 }, action) {
   switch (action.type) {
@@ -10,19 +30,15 @@ function reducer(state = { count: 0 }, action) {
   }
 };
 
-function dispatch(action){
-  state = reducer(state, action);
-  render();
-};
-
 function render() {
   let container = document.getElementById('container');
-  container.textContent = state.count;
+  container.textContent = store.getState().count;
 };
 
-dispatch({ type: '@@INIT' })
+let store = createStore(reducer)
+store.dispatch({ type: '@@INIT' })
 let button = document.getElementById('button');
 
 button.addEventListener('click', function() {
-    dispatch({ type: 'INCREASE_COUNT' });
+    store.dispatch({ type: 'INCREASE_COUNT' });
 })
